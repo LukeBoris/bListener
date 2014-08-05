385eed4c8c3e29e5f1680c3e98d1879523bcef28
Luke Boris <lukeboris@gmail.com>
2014-08-04 23:07:37 -0700

    Fixed some stuff
    Tried some things

--- bListener.js
+++ bListener.js
@@ -1,15 +1,17 @@
 //simulates an event firing
 function eventFire(el, etype){
-  if (el.fireEvent) {
-    (el.fireEvent('on' + etype));
+  if (el.fireEvent) { //check if fireEvent is available on object
+    (el.fireEvent('on' + etype)); //fire the event on the given object
   } else {
-    var evObj = document.createEvent('Events');
-    evObj.initEvent(etype, true, false);
-    el.dispatchEvent(evObj);
+    var evObj = document.createEvent('Events'); //create an Event object
+    evObj.initEvent(etype, true, false); //type of event
+    el.dispatchEvent(evObj); //simulate event
   }
 }
 
-var allLinks = document.links; // create an object of all the links on the page
+var allLinks = document.getElementsByTagName("a"); // create an object of all the links on the page
+
+//fire the event on all links within the allLinks object
 for(var i = 0; i < allLinks.length; i++) {
 	eventFire(allLinks[i], "mousedown");
 }
@@ -23,17 +25,23 @@ function linkChecked(a) {
 	}
 };
 
+//all links
 var x = document.getElementsByTagName("a");
 
 for(var i = 0; i < x.length; i++) {
-	x[i].setAttribute("id", "link" + i);
-	x[i].setAttribute("onMousedown", "linkChecked(this)");
-	x[i].style.border = "dashed";
-	x[i].style.borderColor = "red";
+	if(Number(y) > 0) {
+		x[i].style.border = "dashed";
+		x[i].style.borderColor = "red";
+		x[i].innerHTML = y;
+		x[i].setAttribute("onMousedown", "linkChecked(this)");
+	}
 }
 
 
+
 //Testplace
+//https://developer.chrome.com/extensions/getstarted
+
 
 //newtrial: Try looping through all the event types to see if any of them can be listened for on mousedown
 var worked = function(t) {
